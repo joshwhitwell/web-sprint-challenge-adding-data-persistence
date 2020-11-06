606248742ef410b702d8ffa8923e485b53ca4d27
Joshua Whitwell <josh.whitwell@gmail.com>
2020-11-06 13:18:09 -0500

    added getById endpoint and access method

--- projects/project-model.js
+++ projects/project-model.js
@@ -3,7 +3,19 @@ const db = require('../data/db-config')
 function get() {
     return db('projects')
 }
+function getById(id) {
+    return db('projects')
+        .where({ 'project_id': id })
+        .first()
+}
+
+function insert(project) {
+    return db('projects')
+        .insert(project)
+}
 
 module.exports = {
-    get
+    get,
+    getById,
+    insert
 }
--- projects/project-router.js
+++ projects/project-router.js
@@ -7,7 +7,33 @@ const router = express.Router()
 router.get('/', (req, res) => {
     Projects.get()
         .then(projects => {
-            res.status(200).json({ data: projects })
+            res.status(200).json({ projects })
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    Projects.getById(id)
+        .then(project => {
+            if (!project) {
+                res.status(404).json({ message: `Project with ID ${id} not found` })
+            } else {
+                res.status(200).json({ project })
+            }
+        })
+        .catch (err => {
+            res.status(500).json({ error: err.message })
+        })
+})
+
+router.post('/', (req, res) => {
+    Projects.insert(req.body)
+        .then(() => {
+
+            res.status(201).json({ project })
         })
         .catch(err => {
             res.status(500).json({ error: err.message })
